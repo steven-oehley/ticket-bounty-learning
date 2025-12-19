c9b92cf165289e4e2f117f735825ff7d64e58e7c
Steve <steve.gitprofile@outlook.com>
2025-12-19 19:23:12 +0200

    chore: added ticket status icons

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -2,11 +2,18 @@ import Link from 'next/link';
 import { initialTickets } from '@/data/data';
 import { ticketDetailsPath } from '@/constants/paths';
 
+const TICKET_ICONS = {
+  OPEN: 'O',
+  IN_PROGRESS: '>',
+  DONE: 'X',
+};
+
 const TicketsPage = () => {
   return (
     <div>
       {initialTickets.map((ticket) => (
         <div key={ticket.id}>
+          <div>{TICKET_ICONS[ticket.status]}</div>
           <h2 className="text-lg">{ticket.title}</h2>
 
           <Link className="text-sm underline" href={ticketDetailsPath(ticket.id)}>
--- src/data/data.ts
+++ src/data/data.ts
@@ -4,20 +4,20 @@ export const initialTickets = [
     title: 'Fix login bug',
     content:
       'Users are unable to log in with correct credentials. Investigate the authentication flow and identify any issues with the login API or frontend validation that may be causing this problem. Ensure to check for any recent changes that might have affected the login functionality.',
-    status: 'OPEN',
+    status: 'OPEN' as const,
   },
   {
     id: '2',
     title: 'Add dark mode',
     content:
       'Implement dark mode for better user experience at night. This should include a toggle switch in the settings. Make sure to update all relevant components to support dark mode styling.',
-    status: 'IN_PROGRESS',
+    status: 'IN_PROGRESS' as const,
   },
   {
     id: '3',
     title: 'Update user profile page',
     content:
       'Redesign the user profile page to include more details. Consider adding sections for recent activity, friends list, and user statistics. Ensure the design is responsive and works well on mobile devices.',
-    status: 'DONE',
+    status: 'DONE' as const,
   },
 ];
