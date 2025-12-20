9aa05d67a3e043a61e0c261ccb6ef122f3d17bc3
Steve <steve.gitprofile@outlook.com>
2025-12-20 15:01:08 +0200

    chore: added shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,10 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Figtree } from 'next/font/google';
 import './globals.css';
 import Link from 'next/link';
 import { homePath, ticketsPath } from '@/constants/paths';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
+const figtree = Figtree({ subsets: ['latin'], variable: '--font-sans' });
 
 export const metadata: Metadata = {
   title: 'Ticket Bounty Learning',
@@ -25,8 +17,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+    <html className={`${figtree.variable} antialiased`} lang="en">
+      <body>
         <nav className="supports-backdrop-blur:bg-background/60 bg-background/95 fixed top-0 right-0 left-0 z-20 flex w-full justify-between border-b px-5 py-2.5 backdrop-blur">
           <div>
             <Link className="text-lg font-bold" href={homePath}>
