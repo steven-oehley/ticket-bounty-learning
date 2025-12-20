aded189fefe7afbf65e45c31d8616de5ed35dc51
Steve <steve.gitprofile@outlook.com>
2025-12-20 17:58:55 +0200

    chore: added suppressHydrationWarning for themeprovider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html className={`${figtree.variable} antialiased`} lang="en">
+    <html suppressHydrationWarning className={`${figtree.variable} antialiased`} lang="en">
       <body>
         <ThemeProvider
           disableTransitionOnChange
