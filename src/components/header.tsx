89c6f289882fc9ca14ad09d90780151af2f692f9
Steve <steve.gitprofile@outlook.com>
2025-12-20 20:32:25 +0200

    chore: fix padding issues

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,7 +9,7 @@ import { Button } from './ui/button';
 
 const Header = () => {
   return (
-    <nav className="supports-backdrop-blur:bg-background/60 bg-background/95 fixed top-0 right-0 left-0 z-20 flex w-full justify-between border-b px-5 py-2.5 backdrop-blur">
+    <nav className="supports-backdrop-blur:bg-background/60 bg-background/95 fixed top-0 right-0 left-0 z-20 flex w-full justify-between border-b px-12 py-2.5 backdrop-blur">
       <div>
         <Button asChild variant="ghost">
           <Link href={homePath}>
