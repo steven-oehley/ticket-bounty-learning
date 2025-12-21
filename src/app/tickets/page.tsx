7e28c0206bbcbf334a2c62ee55553149b0d75482
Steve <steve.gitprofile@outlook.com>
2025-12-21 12:08:35 +0200

    feature: added server side fetching

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,13 +1,14 @@
 import { Heading } from '@/components/heading';
-import { initialTickets } from '@/data/data';
 import { TicketItem } from '@/features/ticket/components/ticket-item';
+import { getTickets } from '@/features/ticket/queries/get-tickets';
 
-const TicketsPage = () => {
+const TicketsPage = async () => {
+  const tickets = await getTickets();
   return (
     <div className="flex flex-1 flex-col gap-y-8">
       <Heading description="All your tickets at one place" title="Tickets" />
       <div className="animate-in fade-in slide-in-from-top flex flex-1 flex-col items-center gap-y-4 duration-400">
-        {initialTickets.map((ticket) => (
+        {tickets.map((ticket) => (
           <TicketItem key={ticket.id} ticket={ticket} />
         ))}
       </div>
