4e7394a976b910382c7ee9ffa6c331f89f3f9ad6
Steve <steve.gitprofile@outlook.com>
2025-12-23 12:43:36 +0200

    task: added sort to db query for tickets

--- src/features/ticket/queries/get-tickets.ts
+++ src/features/ticket/queries/get-tickets.ts
@@ -3,6 +3,10 @@ import { prisma } from '@/lib/prisma';
 import { type Ticket } from '../types';
 
 export const getTickets = async (): Promise<Ticket[]> => {
-  const tickets = await prisma.ticket.findMany();
+  const tickets = await prisma.ticket.findMany({
+    orderBy: {
+      createdAt: 'desc',
+    },
+  });
   return tickets;
 };
