f4f982e7c211b723f09c51b6e5ae4f7968e326b3
Steve <steve.gitprofile@outlook.com>
2025-12-26 09:32:54 +0200

    task: improved card-compact

--- src/components/card-compact.tsx
+++ src/components/card-compact.tsx
@@ -1,19 +1,30 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { cn } from '@/lib/utils';
 
 interface CardCompactProps {
   title: string;
   description: string;
+  className?: string;
+  footer?: React.ReactNode;
   children: React.ReactNode;
 }
 
-const CardCompact = ({ title, description, children }: CardCompactProps) => {
+const CardCompact = ({ title, description, footer, className, children }: CardCompactProps) => {
   return (
-    <Card className="w-full max-w-105 self-center">
+    <Card className={cn('w-full max-w-105 self-center', className)}>
       <CardHeader>
         <CardTitle>{title}</CardTitle>
         <CardDescription>{description}</CardDescription>
       </CardHeader>
       <CardContent>{children}</CardContent>
+      {footer && <CardFooter>{footer}</CardFooter>}
     </Card>
   );
 };
