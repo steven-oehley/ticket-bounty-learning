5047ab49809a8923a6360d05b5398d646695f10f
Steve <steve.gitprofile@outlook.com>
2026-01-01 09:49:01 +0200

    chore: added getBaseUrl utility function

---
+++ src/utils/urls.ts
@@ -0,0 +1,17 @@
+export const getBaseUrl = () => {
+  // Know what current env is prod or dev
+  const environment = process.env.NODE_ENV;
+
+  const baseUrl =
+    environment === 'development'
+      ? 'http://localhost:3000'
+      : `https://${process.env.NEXT_PUBLIC_BASE_URL}`;
+
+  return baseUrl;
+};
+
+// Automatically avaiolable env variables from hosting provider, framewrok or node env.
+
+// VERCEL_
+// NEXT_
+// NODE_ENV
