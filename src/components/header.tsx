8b6d71bd4f57a107ea672ade7fa982f551c2d129
Steve <steve.gitprofile@outlook.com>
2026-01-06 14:11:07 +0200

    task: conditionally render header buttons

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,12 +4,23 @@ import { LucideLogOut, LucideTicketCheck } from 'lucide-react';
 
 import { homePath, signInPath, signUpPath, ticketsPath } from '@/constants/paths';
 import { signOut } from '@/features/auth/actions/sign-out';
+import { getAuth } from '@/features/auth/queries/get-auth';
 
 import SubmitBtn from './form/submit-btn';
 import { ThemeToggle } from './theme/theme-toggle';
 import { Button } from './ui/button';
 
-const Header = () => {
+const Header = async () => {
+  // This busts any static rendering because of cookies api
+  // Handle dynamic content then we no longer statically render
+  // cookies() is a dynamic function - Next.js can't know the cookie value at build time, so it forces dynamic rendering
+  // if you wanted some pages static (marketing, docs), you could: Move the header auth check to a client component
+  // client components don't run at build time or on the server request - they run in the browser
+  const { user, session } = await getAuth();
+
+  const isLoggedIn = !!user;
+  console.info(isLoggedIn, user, session);
+
   return (
     <nav className="supports-backdrop-blur:bg-background/60 bg-background/95 fixed top-0 right-0 left-0 z-20 flex w-full justify-between border-b px-12 py-2.5 backdrop-blur">
       <div>
@@ -23,18 +34,27 @@ const Header = () => {
       <div className="align-items flex gap-x-2">
         <ThemeToggle />
         {/* NAV ITEMS */}
-        <Button asChild className="cursor-pointer" variant="default">
-          <Link href={ticketsPath}>Tickets</Link>
-        </Button>
-        <Button asChild className="cursor-pointer" variant="secondary">
-          <Link href={signUpPath}>Sign Up</Link>
-        </Button>
-        <Button asChild className="cursor-pointer" variant="secondary">
-          <Link href={signInPath}>Sign In</Link>
-        </Button>
-        <form action={signOut}>
-          <SubmitBtn icon={<LucideLogOut />} label="Sign Out" variant="ghost" />
-        </form>
+
+        {!isLoggedIn && (
+          <>
+            <Button asChild className="cursor-pointer" variant="secondary">
+              <Link href={signUpPath}>Sign Up</Link>
+            </Button>
+            <Button asChild className="cursor-pointer" variant="default">
+              <Link href={signInPath}>Sign In</Link>
+            </Button>
+          </>
+        )}
+        {isLoggedIn && (
+          <>
+            <Button asChild className="cursor-pointer" variant="default">
+              <Link href={ticketsPath}>Tickets</Link>
+            </Button>
+            <form action={signOut}>
+              <SubmitBtn icon={<LucideLogOut />} label="Sign Out" variant="ghost" />
+            </form>
+          </>
+        )}
       </div>
     </nav>
   );
